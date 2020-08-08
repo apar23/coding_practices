79d5982de53b0597c1dee44bb6b64ab7851cc258
crazyselen <crazyselenophile@gmail.com>
2020-08-09 02:00:49 +0530

    Corrected

--- MaxSumSubarray.cpp
+++ MaxSumSubarray.cpp
@@ -1,15 +1,6 @@
 #include<iostream>
 using namespace std;
-int main()
-{
-    int arr[] = {15,2,4,8,9,5,10,23};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    int sum = 23;
-    subArray(arr,n,sum);
-    return 0;
-}
-
-void subArray(int[]arr, int n, int sum)
+void subArray(int arr[], int n, int sum)
 {
     int i, start=0, current_sum=arr[0];
     for(int i=1 ; i< n; i++)
@@ -19,17 +10,26 @@ void subArray(int[]arr, int n, int sum)
             cout<<"The subarray starts at index"<<start<<"and "<<i-1;
             return;
         }
-        if(current_sum<sum)
-        {
-            current_sum+=arr[i];
-            continue;
-        }
         if(current_sum>sum && start<i-1)
         {
             current_sum-=arr[start];
             start=start+1;
         }
+        if(i<n)
+        {
+            current_sum+=arr[i];
+        }
 
     }
-     cout<<"No subarray exists";
+     cout<<"No sub array exists";
 }
+int main()
+{
+    int arr[] = {15,2,4,8,9,5,10,23};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int sum = 23;
+    subArray(arr,n,sum);
+    return 0;
+}
+
+
